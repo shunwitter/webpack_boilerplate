ae46fd23b03092d3cd2328ef4015de78dbd9282b
shunwitter <polytechnics.shun@gmail.com>
2020-02-05 21:22:58 +0900

    Change webpack compile strategy

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './assets/stylesheets/main.scss';
+
+console.log('This is main.js');
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,95 @@
-const htmlConfig  = require('./webpack/htmlConfig');
-const stylesheetConfig   = require('./webpack/stylesheetConfig');
-const javascriptConfig    = require('./webpack/javascriptConfig');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = [
-  htmlConfig,
-  stylesheetConfig,
-  javascriptConfig,
-];
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'assets/javascripts/[name]-[hash].js',
+  },
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|scss|sass)$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|eot|ttf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]_[hash].[ext]',
+              outputPath: './assets/images',
+               // Hack
+              esModule: false,
+              publicPath: (url, resourcePath, context) => {
+                // https://webpack.js.org/loaders/file-loader/#publicpath
+                // url:           hashed file name
+                // resourcePath:  absolute path of original file
+                // context:       webpack directory
+                if (/\/images\//.test(resourcePath)) {
+                  return `/assets/images/${url}`;
+                }
+                if (/\/fonts\//.test(resourcePath)) {
+                  return `/assets/fonts/${url}`;
+                }
+                return `/assets/${url}`;
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'assets/stylesheets/[name]-[hash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/index.pug',
+      filename: 'index.html',
+      minify: false,
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/posts/1.pug',
+      filename: 'posts/1.html',
+      minify: false,
+    }),
+  ],
+}
